0e8d4f4c838f7afe43f61ac517dac3adb4ec4d4c
Mark Promislow <MarkPromislow@gmail.com>
2018-01-15 22:30:28 -0500

    Update IntrusiveObjectPool.h

--- IntrusiveObjectPool.h
+++ IntrusiveObjectPool.h
@@ -9,53 +9,67 @@ template <typename BaseType, typename Type>
 class ObjectPool
 {
 protected:
-	size_t _blockSize;
 	BaseType *_next;
+	size_t _blockSize;
 
 	struct Block
 	{
-		size_t _size;
 		Block *_next;
+		size_t _blockSize;
 		Type *_objects;
 
-		void initialize(size_t size)
-		{
-			_size = size;
-			_objects = new (this + 1) Type[_size];
-			Type *itr = _objects;
-			for (Type *end = _objects + _size - 1; itr < end; ++itr)
-				itr->_next = itr + 1;
-			itr->_next = 0;
-		}
+		Type* initialize(size_t size);
 
-		~Block()
-		{
-			for (Type *itr = _objects, *end = _objects + _size; itr < end; ++itr)
-				itr->~Type();
-		}
+		~Block();
 	};
 	Block *_blocks;
 public:
-	ObjectPool(size_t blockSize = 256) : _blockSize(blockSize), _next(0), _blocks(0) {}
+	ObjectPool(size_t blockSize = 256) : _next(0), _blockSize(blockSize), _blocks(0) {}
 
 	Type *allocate();
 	void free(Type *object);
+
+	size_t blockCnt() const;
+	size_t blockSize() const { return _blockSize; }
+	void setBlockSize(size_t blockSize) { _blockSize = blockSize; }
 
 	~ObjectPool();
 };
 
 template <typename BaseType, typename Type>
+Type* ObjectPool<BaseType, Type>::Block::initialize(size_t size)
+{
+	_blockSize = size;
+	Type *itr;
+	itr = _objects = new (static_cast<void*>(this + 1)) Type[_blockSize];
+	for (Type *end = _objects + _blockSize - 1; itr < end; ++itr)
+		static_cast<BaseType*>(itr)->_next = itr + 1;
+	static_cast<BaseType*>(itr)->_next = 0;
+	return _objects;
+}
+
+template <typename BaseType, typename Type>
+ObjectPool<BaseType, Type>::Block::~Block()
+{
+	for (Type *itr = _objects, *end = _objects + _blockSize; itr < end; ++itr)
+		itr->~Type();
+}
+
+template <typename BaseType, typename Type>
 Type* ObjectPool<BaseType, Type>::allocate()
 {
-	BaseType *object = _next;
-	if (! object)
+	BaseType *object;
+	if (! (object = _next))
 	{
+#if _MSC_VER
 		Block *block = static_cast<Block*>(operator new (sizeof(Block) + sizeof(size_t) + _blockSize * sizeof(Type)));
+#else
+		Block *block = static_cast<Block*>(operator new (sizeof(Block) + _blockSize * sizeof(Type)));
+#endif
 		block->_next = _blocks;
 		_blocks = block;
 
-		block->initialize(_blockSize);
-		object = block->_objects;
+		object = block->initialize(_blockSize);
 	}
 	_next = object->_next;
 	return static_cast<Type*>(object);
@@ -69,10 +83,19 @@ void ObjectPool<BaseType, Type>::free(Type *object)
 }
 
 template <typename BaseType, typename Type>
+size_t ObjectPool<BaseType, Type>::blockCnt() const
+{
+	size_t cnt(0);
+	for (Block *block = _blocks; block; block = block->_next) ++cnt;
+	return cnt;
+}
+
+template <typename BaseType, typename Type>
 ObjectPool<BaseType, Type>::~ObjectPool()
 {
-	for (Block *block = _blocks; block; block = _blocks)
+	for (; _blocks;)
 	{
+		Block *block = _blocks;
 		_blocks = block->_next;
 		delete block;
 	}
